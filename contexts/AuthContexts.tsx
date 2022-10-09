4e820be360954719ff07cf2cc586c96c218629ef
Rodrigo Camargo <rodrigocamargo854@gmail.com>
2022-10-09 19:52:48 -0300

    added authorization before redirect to dashboards

--- contexts/AuthContexts.tsx
+++ contexts/AuthContexts.tsx
@@ -1,12 +1,13 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { api } from "../services/api";
 import Router from "next/router";
+import { setCookie, parseCookies } from "nookies";
 
-type User={
- email:string
- permissions:string[]
- roles:string[]
-}
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
 
 type SignIncredentials = {
   email: string;
@@ -15,7 +16,7 @@ type SignIncredentials = {
 
 type AuthContextData = {
   signIn(credentials: SignIncredentials): Promise<void>;
-  user:User
+  user: User;
   isAuthenticated: boolean;
 };
 type Props = {
@@ -26,9 +27,24 @@ type Props = {
 export const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({ children }: Props) {
-const [user,setUser] = useState<User>()
+  const [user, setUser] = useState<User>();
 
   const isAuthenticated = !!user;
+
+  useEffect(() => {
+    const { "nextauth.token": token } = parseCookies(); // bring all saves cookies
+    if (token) {
+      api.get("/me").then((response) => {
+        const { email, pemissions, roles } = response.data;
+
+        setUser({
+          email,
+          pemissions,
+          roles,
+        });
+      });
+    }
+  }, []);
 
   async function signIn({ email, password }: SignIncredentials) {
     try {
@@ -37,24 +53,37 @@ const [user,setUser] = useState<User>()
         password,
       });
 
-      const{permissions,roles} = response.data
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      setCookie(undefined, "nextAuth.token", token, {
+        maxAge: 60 * 60 * 24 * 30, //30 days
+        path: "/",
+      });
+
+      setCookie(undefined, "nextAuth.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, //30 days
+        path: "/",
+      });
 
       setUser({
         email,
         permissions,
-        roles
-      })
+        roles,
+      });
 
-      Router.push('/Dashboards')
+      //before redirect to dashboard , doing the authorization 
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+      Router.push("/Dashboards");
 
       console.log(response.data);
     } catch (err) {
       console.log(err);
     }
   }
-  
+
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated,user }}>
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   );
--- pages/Dashboards.tsx
+++ pages/Dashboards.tsx
@@ -1,21 +1,25 @@
+import Router from "next/router";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../contexts/AuthContexts";
+import { api } from "../services/api";
 
-import Router from "next/router"
-import { useContext } from "react"
-import { AuthContext } from "../contexts/AuthContexts"
-
-function handleBackHome(){
-    return Router.push('/')
+function handleBackHome() {
+  return Router.push("/");
 }
 
-export default function Dashboards(){
+export default function Dashboards() {
+  useEffect(() => {
+    api.get("/me").then((response) => {
+      console.log(response);
+    });
+  }, []);
+  
+  const { user } = useContext(AuthContext);
 
-    const {user} = useContext(AuthContext)
-
-
-    return (
-        <>
-        <h1>Hello mr{user?.email}</h1>
-        <button onClick={handleBackHome}> home</button>
-        </>
-    )
+  return (
+    <>
+      <h1>Hello mr {user?.email}</h1>
+      <button onClick={handleBackHome}> home</button>
+    </>
+  );
 }
