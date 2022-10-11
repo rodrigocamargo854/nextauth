83a723fa60567031be61beaa62b48c2e1cb0fc57
Rodrigo Camargo <rodrigocamargo854@gmail.com>
2022-10-11 06:22:05 -0300

    inserted logic to refresh token

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,15 @@
+import { rejects } from "assert";
 import axios, { AxiosError } from "axios";
-import { parseCookies, setCookie } from "nookies";
+import { Router } from "next/router";
+import { destroyCookie, parseCookies, setCookie } from "nookies";
+import { signOut } from "../contexts/AuthContexts";
 
 let cookies = parseCookies();
+let isRefreshing = false;
+let failedRequestsQueue: {
+  resolve: (token: string) => void;
+  reject: (err: AxiosError<unknown, any>) => void;
+}[] = [];
 
 export const api = axios.create({
   baseURL: "http://localhost:3333",
@@ -17,37 +25,67 @@ api.interceptors.response.use(
   },
   (error: AxiosError) => {
     if (error.response?.status === 401) {
-      if (error.response.data?.code === "token.expired" ) {
+      if (error.response.data?.code === "token.expired") {
         cookies = parseCookies();
 
         const { "nextauth.refreshToken": refreshToken } = cookies;
-        api
-          .post("/refresh", {
-            refreshToken,
-          })
-          .then((response) => {
-            const { token } = response.data;
+        const originalConfig = error.config;
 
-            setCookie(undefined, "nextAuth.token", token, {
-              maxAge: 60 * 60 * 24 * 30, //30 days
-              path: "/",
-            });
+        if (!refreshToken) {
+          isRefreshing = true;
 
-            setCookie(
-              undefined,
-              "nextAuth.refreshToken",
-              response.data.refreshToken,
-              {
+          api
+            .post("/refresh", {
+              refreshToken,
+            })
+            .then((response) => {
+              const { token } = response.data;
+
+              setCookie(undefined, "nextAuth.token", token, {
                 maxAge: 60 * 60 * 24 * 30, //30 days
                 path: "/",
-              }
-            );
+              });
 
-            api.defaults.headers["Authorization"] = `Bearer${token}`;
+              setCookie(
+                undefined,
+                "nextAuth.refreshToken",
+                response.data.refreshToken,
+                {
+                  maxAge: 60 * 60 * 24 * 30, //30 days
+                  path: "/",
+                }
+              );
+
+              api.defaults.headers["Authorization"] = `Bearer${token}`;
+
+              failedRequestsQueue.forEach((request) => request.resolve(token));
+              failedRequestsQueue = [];
+            })
+            .catch((err) => {
+              failedRequestsQueue.forEach((request) => request.reject(err));
+              failedRequestsQueue = [];
+            })
+            .finally(() => {
+              isRefreshing = false;
+            });
+        }
+
+        return new Promise((resolve, reject) => {
+          failedRequestsQueue.push({
+            resolve: (token: string) => {
+              originalConfig.headers["Authorization"] = `Bearer ${token}`;
+
+              resolve(api(originalConfig));
+            },
+            reject: (err: AxiosError) => {
+              reject(err);
+            },
           });
+        });
       } else {
         //deslogar o usuario
+        signOut();
       }
     }
-  }
-);
+    return Promise.reject(error);
+  });
